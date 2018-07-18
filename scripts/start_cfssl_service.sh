fbbbe3bf0d7206e2372268c3c3e7efef2497c78e
Eric Guinois <eric.guinois@gatas.ca>
2018-07-18 09:16:47 -0400

    Loglevel

--- scripts/start_cfssl_service.sh
+++ scripts/start_cfssl_service.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 #cfssl serve -address=${CFSSL_ADDRESS} -port=8888 -config=${CFSSL_CA_POLICY_FILE} -ca-bundle=/etc/cfssl/ca-bundle.crt -int-bundle=/etc/cfssl/data/server-bundle.pem -int-dir=/etc/cfssl/data/intermediates -db-config=/etc/cfssl/data/db-config.json -responder=/etc/cfssl/data/ocsp/ocsp.pem -responder-key=/etc/cfssl/data/ocsp/ocsp-key.pem -ca=/etc/cfssl/data/server.pem -ca-key=/etc/cfssl/data/server-key.pem 
-cfssl serve -address=${CFSSL_ADDRESS} -port=8888 -config=${CFSSL_CA_POLICY_FILE} -ca-bundle=/etc/cfssl/data/server-bundle.pem -int-dir=/etc/cfssl/data/intermediates -db-config=/etc/cfssl/data/db-config.json -responder=/etc/cfssl/data/ocsp/ocsp.pem -responder-key=/etc/cfssl/data/ocsp/ocsp-key.pem -ca=/etc/cfssl/data/server.pem -ca-key=/etc/cfssl/data/server-key.pem
+cfssl serve -address=${CFSSL_ADDRESS} -port=8888 -config=${CFSSL_CA_POLICY_FILE} -ca-bundle=/etc/cfssl/data/server-bundle.pem -int-dir=/etc/cfssl/data/intermediates -db-config=/etc/cfssl/data/db-config.json -responder=/etc/cfssl/data/ocsp/ocsp.pem -responder-key=/etc/cfssl/data/ocsp/ocsp-key.pem -ca=/etc/cfssl/data/server.pem -ca-key=/etc/cfssl/data/server-key.pem -loglevel=0
 
 #exec nginx -c /etc/nginx/nginx.conf
 
